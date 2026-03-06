55f26f3f8482395047ed621ad8be19422b1c5cb2
Deep1345 <101928871+Deep1345@users.noreply.github.com>
2026-03-06 13:46:48 +0530

    Update test_calculator.cpp

--- test_calculator.cpp
+++ test_calculator.cpp
@@ -2,22 +2,34 @@
 #include "calculator.h"
 
 TEST(CalculatorTest, SquareRootTest) {
-    EXPECT_EQ(squareRoot(25),5);
+    EXPECT_DOUBLE_EQ(squareRoot(25), 5);
 }
 
 TEST(CalculatorTest, FactorialTest) {
-    EXPECT_EQ(factorial(5),120);
+    EXPECT_EQ(factorial(5), 120);
 }
 
-TEST(CalculatorTest, LogTest) {
-    EXPECT_NEAR(naturalLog(1),0,0.001);
+TEST(CalculatorTest, NaturalLogTest) {
+    EXPECT_NEAR(naturalLog(1), 0, 0.001);
 }
 
 TEST(CalculatorTest, PowerTest) {
-    EXPECT_EQ(power(2,3),8);
+    EXPECT_DOUBLE_EQ(power(2, 3), 8);
 }
 
-int main(int argc,char **argv){
-    testing::InitGoogleTest(&argc,argv);
+TEST(CalculatorTest, NegativeSquareRootTest) {
+    EXPECT_THROW(squareRoot(-4), std::invalid_argument);
+}
+
+TEST(CalculatorTest, NegativeFactorialTest) {
+    EXPECT_THROW(factorial(-3), std::invalid_argument);
+}
+
+TEST(CalculatorTest, InvalidLogTest) {
+    EXPECT_THROW(naturalLog(0), std::invalid_argument);
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
