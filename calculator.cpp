ead2f56045d63b1f394ed4b65a078533f7f0399c
Deep1345 <101928871+Deep1345@users.noreply.github.com>
2026-03-06 13:39:41 +0530

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -1,80 +1,24 @@
-#include <iostream>
 #include <cmath>
+#include "calculator.h"
 
-using namespace std;
+double squareRoot(double x) {
+    return sqrt(x);
+}
 
-int main() {
+int factorial(int n) {
+    int result = 1;
+    for(int i=1;i<=n;i++)
+        result *= i;
+    return result;
+}
 
-    int choice;
-    double x, b;
-    int n;
+double naturalLog(double x) {
+    return log(x);
+}
 
-    while (true) {
-
-        cout << "\n===== Scientific Calculator =====\n";
-        cout << "1. Square Root (√x)\n";
-        cout << "2. Factorial (x!)\n";
-        cout << "3. Natural Log (ln x)\n";
-        cout << "4. Power (x^b)\n";
-        cout << "5. Exit\n";
-        cout << "Enter your choice: ";
-
-        cin >> choice;
-
-        if (choice == 1) {
-            cout << "Enter number: ";
-            cin >> x;
-
-            if (x < 0)
-                cout << "Square root not defined for negative numbers\n";
-            else
-                cout << "Result = " << sqrt(x) << endl;
-        }
-
-        else if (choice == 2) {
-            cout << "Enter integer: ";
-            cin >> n;
-
-            if (n < 0) {
-                cout << "Factorial not defined for negative numbers\n";
-            }
-            else {
-                long long fact = 1;
-                for (int i = 1; i <= n; i++)
-                    fact = fact * i;
-
-                cout << "Result = " << fact << endl;
-            }
-        }
-
-        else if (choice == 3) {
-            cout << "Enter number (>0): ";
-            cin >> x;
-
-            if (x <= 0)
-                cout << "Log undefined\n";
-            else
-                cout << "Result = " << log(x) << endl;
-        }
-
-        else if (choice == 4) {
-            cout << "Enter x: ";
-            cin >> x;
-            cout << "Enter b: ";
-            cin >> b;
-
-            cout << "Result = " << pow(x, b) << endl;
-        }
-
-        else if (choice == 5) {
-            cout << "Exiting calculator\n";
-            break;
-        }
-
-        else {
-            cout << "Invalid choice\n";
-        }
-    }
+double power(double x, double b) {
+    return pow(x,b);
+}
 
     return 0;
 }
